f124de89cd347d27385507886cda5d723beb723d
Nicu <neon-doubt@maskmail.net>
2020-03-19 20:04:11 +0200

    Using now the official taskforce-fe-components (#13)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import "taskforce-fe-components/dist/index.css";
+import "@code4ro/taskforce-fe-components/dist/index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
