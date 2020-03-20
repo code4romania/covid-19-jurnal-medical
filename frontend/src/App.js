d436cb80416d84c8ac859a67f280abab70ae014b
Nicu <neon-doubt@maskmail.net>
2020-03-20 23:48:01 +0200

    feat(header): Added app's header (#32)
    * feat(header): Added app's header
    
    * feat(header): Moved header to it's own component
    
    * feat(header): Added ProfileItems and small polishes
    
    * feat(header): Fixed external links
    
    * feat(header): Switched incubated with the proper developed section
    
    * feat(header): Fixed logo not showing in chrome, updated shared components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,19 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/despre" component={About} />
-    </Switch>
+    <>
+      <Header />
+      <div className="container">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/despre" component={About} />
+        </Switch>
+      </div>
+    </>
   );
 }
 
