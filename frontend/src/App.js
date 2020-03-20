629c31fe1341b47b985cb9dc6a7d7ac611706dea
Surdu <neon-doubt@maskmail.net>
2020-03-20 20:16:31 +0200

    feat(header): Added app's header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,37 @@
 import React from "react";
 import "./App.css";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, NavLink } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
+import { ReactComponent as LogoSvg } from "./assets/stamacasa.svg";
+
+import { Header } from "@code4ro/taskforce-fe-components";
 
 function App() {
+  const Logo = () => (
+    <NavLink to="/">
+      <LogoSvg />
+    </NavLink>
+  );
+
+  const MenuItems = () => (
+    <>
+      <NavLink to="/despre">Despre</NavLink>
+      <NavLink to="https://code4.ro/ro/apps/">Ecosistemul Covid-19</NavLink>
+      <NavLink to="https://code4.ro/ro/doneaza/">Sprijină proiectul</NavLink>
+    </>
+  );
+
   return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/despre" component={About} />
-    </Switch>
+    <>
+      <Header Logo={<Logo />} MenuItems={<MenuItems />} />
+      <div className="container">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/despre" component={About} />
+        </Switch>
+      </div>
+    </>
   );
 }
 
