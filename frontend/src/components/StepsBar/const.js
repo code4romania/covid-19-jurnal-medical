89aa5ac9d73c4573bda300bc80ab3391b990c7c5
Paul Ghiran <webdev@paulghiran.com>
2020-03-21 05:21:45 +0200

    Git 16: Added Steps bar & minimal router

---
+++ frontend/src/components/StepsBar/const.js
@@ -0,0 +1,16 @@
+import { ROUTES } from "../AppRouter/const";
+
+export const STEPS_CONFIG = [
+  {
+    label: "Contul Tău",
+    URL: ROUTES.account.path
+  },
+  {
+    label: "Completează formularul de evaluare",
+    URL: ROUTES.selfevaluation.path
+  },
+  {
+    label: "Adaugă-ți familia în cont",
+    URL: ROUTES.addmember.path
+  }
+];
