2796255040c1ed1fd78636c36389316a4aad43b9
Oana <oanamuntean8@gmail.com>
2020-03-22 00:11:30 +0200

    Use login form from shared components

--- frontend/src/components/Login/index.js
+++ frontend/src/components/Login/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { Button } from "@code4ro/taskforce-fe-components";
+import { LoginForm } from "@code4ro/taskforce-fe-components";
 
 import { ReactComponent as LogoSvg } from "../../assets/stamacasa.svg";
 
@@ -13,23 +13,7 @@ const Login = () => {
         <NavLink to="/">
           <LogoSvg />
         </NavLink>
-        <h1>LOGIN</h1>
-        <form className="login-form">
-          <label>
-            <span>Email</span>
-            <input type="email" required />
-          </label>
-          <label>
-            <span>Password</span>
-            <input type="password" minLength="5" required />
-          </label>
-          <div className="button-wrapper">
-            <Button>Login</Button>
-            <NavLink to="/">
-              <span className="forgot-password">Ai uitat parola?</span>
-            </NavLink>
-          </div>
-        </form>
+        <LoginForm />
       </div>
     </div>
   );
