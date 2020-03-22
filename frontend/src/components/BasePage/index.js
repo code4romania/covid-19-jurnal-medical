ce8b7cc588c2cae576dbf40c57cbf3cba5ef259e
Surdu <neon-doubt@maskmail.net>
2020-03-22 15:56:10 +0200

    Moved hero and steps bar out of base page component

--- frontend/src/components/BasePage/index.js
+++ frontend/src/components/BasePage/index.js
@@ -2,23 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import Header from "../Header";
-import { Hero } from "@code4ro/taskforce-fe-components";
-import StepsBar from "../StepsBar";
 import Footer from "../Footer";
 
 const BasePage = ({ children }) => {
   return (
     <>
       <Header />
-      <div className="container main-content">
-        <Hero
-          title="Ce pași ai de urmat"
-          subtitle="Pentru a te putea ajuta iata ce ai la dispozitie in contul tau:"
-          useFallbackIcon={true}
-        />
-        <StepsBar />
-        {children}
-      </div>
+      <div className="container main-content">{children}</div>
       <Footer />
     </>
   );
--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 
+import { Hero } from "@code4ro/taskforce-fe-components";
+import StepsBar from "../StepsBar";
 import BasePage from "../BasePage";
 
 const Home = () => {
-  return <BasePage>Home placeholder</BasePage>;
+  return (
+    <BasePage>
+      <Hero
+        title="Ce pași ai de urmat"
+        subtitle="Pentru a te putea ajuta iata ce ai la dispozitie in contul tau:"
+        useFallbackIcon={true}
+      />
+      <StepsBar />
+      Home placeholder
+    </BasePage>
+  );
 };
 
 export default Home;
