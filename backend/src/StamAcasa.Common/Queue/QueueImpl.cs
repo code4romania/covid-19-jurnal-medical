4b188e29a9593dbee3990bbd89f40b9d31f26ad5
Cosmin Popescu <cosmin.popescu93@gmail.com>
2020-03-28 12:54:13 +0200

    added a basic implementation of IQueue with INotification injected

---
+++ backend/src/StamAcasa.Common/Queue/QueueImpl.cs
@@ -0,0 +1,30 @@
+﻿using StamAcasa.Common.Notifications;
+using StamAcasa.Common.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StamAcasa.Common.Queue
+{
+    class QueueImpl : IQueue
+    {
+
+        private readonly INotification _notification;
+
+        public QueueImpl(INotification notification)
+        {
+            _notification = notification;
+        }
+
+        public Task<INotification> Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Queue(INotification notification)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
