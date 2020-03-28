64e27586fb73e7ea598361dcfb04f35e94bbc251
Cristian Mardare <cristian.mardare.dev@gmail.com>
2020-03-28 16:11:23 +0200

    feat(email:builder): [WIP] first version of email builder Template is loaded from disk Placeholders are as %%PARAMETER%% Parameters are received in dictionary as: { PARAMETER: value }
    Overview:
    The application is a background worker that will subscribe to a RabbitMQ queue
    When requests come, an email will be built using predefined templates.
    The email will be sent using a configurable SMTP server

---
+++ backend/src/StamAcasa.EmailService/EmailBuilder/Models/EmailModel.cs
@@ -0,0 +1,9 @@
+﻿namespace StamAcasa.EmailService.EmailBuilder.Models
+{
+    public class EmailModel
+    {
+        public string Address { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+    }
+}
