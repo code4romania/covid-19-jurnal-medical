4449f8dc79c7d189a075c5761d07279f3ccc79db
trupci <andreea.trupcea@ing.com>
2020-03-28 17:21:50 +0200

    * Moved Identity to separate area * Added implementations for email system * Tested confirmation email using SendGrid(you need to configure a user and api key as user secrets to test) * registration addres is /identity/account/register

--- backend/src/StamAcasa.IdentityServer/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ backend/src/StamAcasa.IdentityServer/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -41,20 +41,20 @@ namespace IdentityServer.Pages.Account
                 return NotFound($"Unable to load user with email '{email}'.");
             }
 
-            Email = email;
-            // Once you add a real email sender, you should remove this code that lets you confirm the account
-            DisplayConfirmAccountLink = true;
-            if (DisplayConfirmAccountLink)
-            {
-                var userId = await _userManager.GetUserIdAsync(user);
-                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                EmailConfirmationUrl = Url.Page(
-                    "/Account/ConfirmEmail",
-                    pageHandler: null,
-                    values: new { area = "Identity", userId = userId, code = code },
-                    protocol: Request.Scheme);
-            }
+            //Email = email;
+            //// Once you add a real email sender, you should remove this code that lets you confirm the account
+            //DisplayConfirmAccountLink = true;
+            //if (DisplayConfirmAccountLink)
+            //{
+            //    var userId = await _userManager.GetUserIdAsync(user);
+            //    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            //    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+            //    EmailConfirmationUrl = Url.Page(
+            //        "/Account/ConfirmEmail",
+            //        pageHandler: null,
+            //        values: new { area = "Identity", userId = userId, code = code },
+            //        protocol: Request.Scheme);
+            //}
 
             return Page();
         }
---
+++ backend/src/StamAcasa.IdentityServer/Services/SmtpSenderOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace StamAcasa.IdentityServer.Services
+{
+    public class SmtpSenderOptions
+    {
+        public string SmtpServer { get; set; }
+        public string SmtpUser { get; set; }
+        public string SmtpPassword { get; set; }
+    }
+}
