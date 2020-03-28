bdee322dad464fe5bf205b30f88a163850087d71
Paul Marculescu <paulmarculescu@gmail.com>
2020-03-28 13:04:09 +0200

    Fixed typo: Assesment -> Assessment #51

--- backend/src/StamAcasa.Common/Notifications/AssessmentNotification.cs
+++ backend/src/StamAcasa.Common/Notifications/AssessmentNotification.cs
@@ -3,13 +3,13 @@ using StamAcasa.Common.Models;
 
 namespace StamAcasa.Common.Notifications
 {
-    public class AssesmentNotification : INotification
+    public class AssessmentNotification : INotification
     {
         public Guid Id { get; }
 
         private UserInfo UserInfo { get; }
 
-        public AssesmentNotification(UserInfo userInfo)
+        public AssessmentNotification(UserInfo userInfo)
         {
             Id = Guid.NewGuid();
             UserInfo = userInfo;
