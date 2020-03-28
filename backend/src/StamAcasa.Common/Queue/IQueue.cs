38d1a0ff4a50c2155f8cbd4f6a84b53e524fe838
Cosmin Popescu <cosmin.popescu93@gmail.com>
2020-03-28 12:41:56 +0200

    added IQueue

---
+++ backend/src/StamAcasa.Common/Queue/IQueue.cs
@@ -0,0 +1,15 @@
+﻿using StamAcasa.Common.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StamAcasa.Common.Services
+{
+    interface IQueue
+    {
+        void Queue(INotification notification);
+
+        Task<INotification> Dequeue();
+    }
+}
