a514f05838b7d9ee579eadf7610fe4dea1afec9e
trupci <trupci@gmail.com>
2020-03-28 17:54:04 +0200

    * Moved Identity to separate area (#60)
    * Added implementations for email system
    * Tested confirmation email using SendGrid(you need to configure a user and api key as user secrets to test)
    * registration addres is /identity/account/register
    
    Co-authored-by: trupci <andreea.trupcea@ing.com>

---
+++ backend/src/StamAcasa.IdentityServer/Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StamAcasa.IdentityServer.Services {
+    public class AuthMessageSenderOptions {
+        public string SendGridUser { get; set; }
+        public string SendGridKey { get; set; }
+    }
+}
---
+++ backend/src/StamAcasa.IdentityServer/Services/EmailSenderSmtp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace StamAcasa.IdentityServer.Services {
+    public class EmailSenderSmtp : IEmailSender {
+        private readonly SmtpSenderOptions _options;
+        private string _adminEmail;
+
+        public EmailSenderSmtp(IOptions<SmtpSenderOptions> options, IConfiguration config)
+        {
+            _options = options.Value;
+            _adminEmail = config.GetValue<string>("AdminEmailAddress");
+        }
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var client = new SmtpClient(_options.SmtpServer)
+            {
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(_options.SmtpUser, _options.SmtpPassword)
+            };
+
+            var mailMessage = new MailMessage {From = new MailAddress(_adminEmail) };
+            mailMessage.To.Add(email);
+            mailMessage.Body = htmlMessage;
+            mailMessage.Subject = subject;
+            client.Send(mailMessage);
+        }
+    }
+}
