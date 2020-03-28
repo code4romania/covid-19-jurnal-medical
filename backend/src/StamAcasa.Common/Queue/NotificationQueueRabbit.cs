5af8db671e73489f1634a100b171c660424a1539
Paul Marculescu <paulmarculescu@gmail.com>
2020-03-28 16:02:11 +0200

    Rename NotificationQueue to NotificationQueueRabbit; make construction dependent on RabbitMQ parameters #51

---
+++ backend/src/StamAcasa.Common/Queue/NotificationQueueRabbit.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using StamAcasa.Common.Notifications;
+using StamAcasa.Common.Services;
+
+namespace StamAcasa.Common.Queue
+{
+    public class NotificationQueueRabbit : IQueue
+    {
+        private string HostName { get; }
+
+        private string QueueName { get; }
+
+        private string Exchange { get; }
+
+        private string RoutingKey { get; }
+
+        public NotificationQueueRabbit(string hostName, string queueName, string exchange, string routingKey)
+        {
+            HostName = hostName;
+            QueueName = queueName;
+            Exchange = exchange;
+            RoutingKey = routingKey;
+        }
+
+        public Task<INotification> Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Queue(INotification notification)
+        {
+            var factory = new ConnectionFactory() { HostName = HostName };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: QueueName,
+                         durable: true,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+                    var body = notification.GetBytes();
+
+                    channel.BasicPublish(exchange: Exchange,
+                                     routingKey: RoutingKey,
+                                     basicProperties: null,
+                                     body: body);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
