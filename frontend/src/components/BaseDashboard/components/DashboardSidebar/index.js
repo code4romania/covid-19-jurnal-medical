607273da3382ba7631ba195e7a1ce15366fe3f7a
maloun96 <malai9696@gmail.com>
2020-04-02 11:57:26 +0300

    Fix header style, remove position fixed for sidebar

--- frontend/src/components/BaseDashboard/components/DashboardSidebar/index.js
+++ frontend/src/components/BaseDashboard/components/DashboardSidebar/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./DashboardSidebar.scss";
 import dashboard_routes from "../../dashboard-routes";
 import { NavLink } from "react-router-dom";
-import classNames from "classnames";
 
 const DashboardSidebar = () => {
   const instrumentsItems = dashboard_routes.map((item, index) => (
@@ -11,7 +10,7 @@ const DashboardSidebar = () => {
       key={index}
       to={item.path}
       activeClassName="dashboardActiveLink"
-      className={classNames(item.className, "dashboardLink")}
+      className={[item.className, "dashboardLink"].join(" ")}
     >
       {item.name}
     </NavLink>
--- frontend/src/components/BaseDashboard/index.js
+++ frontend/src/components/BaseDashboard/index.js
@@ -1,41 +1,32 @@
 import React from "react";
-import { Header as TFHeader } from "@code4ro/taskforce-fe-components";
-import { NavLink, Route } from "react-router-dom";
-import { ReactComponent as LogoSvg } from "../../assets/stamacasa.svg";
+import { Route } from "react-router-dom";
 import "./BaseDashboard.scss";
 import DashboardSidebar from "./components/DashboardSidebar";
 import dashboard_routes from "./dashboard-routes";
+import DashboardHeader from "./components/DashboardHeader";
 
 const BaseDashboard = () => {
-  const Logo = () => (
-    <NavLink to="/">
-      <LogoSvg />
-    </NavLink>
-  );
-  const MenuItems = () => (
-    <div className={"headerItems"}>
-      <input type={"text"} placeholder={"Cauta"} className={"searchInput"} />
-      <span className="account-items">User</span>
-    </div>
-  );
-
   return (
     <div className="dashboard-wrapper">
       <div className={"header-wrapper"}>
-        <TFHeader Logo={<Logo />} MenuItems={<MenuItems />} />
+        <DashboardHeader />
       </div>
       <div className="container-fluid">
         <div className="columns main-section">
-          <DashboardSidebar />
-          <div className="main">
-            {dashboard_routes.map(({ path, component, extraProps }) => (
-              <Route
-                path={path}
-                component={component}
-                key={path}
-                {...extraProps}
-              />
-            ))}
+          <div className="column is-one-quarter is-paddingless">
+            <DashboardSidebar />
+          </div>
+          <div className="column is-paddingless">
+            <div className="main">
+              {dashboard_routes.map(({ path, component, extraProps }) => (
+                <Route
+                  path={path}
+                  component={component}
+                  key={path}
+                  {...extraProps}
+                />
+              ))}
+            </div>
           </div>
         </div>
       </div>
