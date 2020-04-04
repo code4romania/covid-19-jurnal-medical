c3f6cfac977c373297a8401d8b88fc1c57914979
Daniel Miu <miu.daniel@gmail.com>
2020-04-04 20:24:28 +0300

    add output for dns
    As the DNS is always changed when a new deploy is triggered, we can now show in console the DNS records

--- aws_deploy/output.tf
+++ aws_deploy/output.tf
@@ -1,10 +1,19 @@
 output "NAT_Egress_Elastic_IP" {
   value = aws_eip.private.*.public_ip
 }
-/*
-output "DNS" {
-  value = module.load-balancer.dns
+
+output "DNS Front-End" {
+  value = module.front-end.dns
 }
-*/
 
+output "DNS API" {
+  value = module.api.dns
+}
 
+output "DNS identitysrv" {
+  value = module.identitysrv.dns
+}
+
+output "DNS Postgres" {
+  value = module.postgres.dns
+}
