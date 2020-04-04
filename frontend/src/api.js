87ea7ca51e7f1914e12e15c251e7991f58f2950b
Mircea M <mircea.moise@gmail.com>
2020-04-04 17:35:53 +0100

    Fixing issue with authentication

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,8 +2,9 @@ import axios from "axios";
 import { AuthService } from "./auth";
 
 const api = axios.create();
-api.interceptors.request.use(config => {
-  const token = AuthService.user ? AuthService.user.access_token : null;
+api.interceptors.request.use(async config => {
+  const user = await AuthService.loadUser();
+  const token = user ? user.access_token : null;
   if (token) {
     return {
       ...config,
