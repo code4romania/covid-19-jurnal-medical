3f77e44e1b5c5c5c68a404b134395453cc88af13
adascaliteiradu <adascalitei.radu@live.com>
2020-04-04 20:27:16 +0300

    chore: load user from store

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 import BasePage from "../BasePage";
 import UserHomePage from "./components/UserHomePage";
 import DefaultHomePage from "./components/DefaultHomePage";
+import { connect } from "react-redux";
 
-const Home = () => {
-  const isAuthenticated = false;
+const Home = ({ user }) => {
+  const isAuthenticated = user !== null;
   const page = isAuthenticated ? (
     <UserHomePage></UserHomePage>
   ) : (
@@ -13,4 +15,14 @@ const Home = () => {
   return <BasePage>{page}</BasePage>;
 };
 
-export default Home;
+const mapStateToProps = state => {
+  return {
+    user: state.UserReducer.user
+  };
+};
+
+Home.propTypes = {
+  user: PropTypes.any
+};
+
+export default connect(mapStateToProps)(Home);
