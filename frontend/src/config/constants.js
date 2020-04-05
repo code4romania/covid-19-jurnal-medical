21511be0a6fbda3f011fe87cb13ea57bd6bc2892
Cristi Habliuc <cristi.habliuc@gmail.com>
2020-04-05 17:01:02 +0300

    fixed lint issues

--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -6,22 +6,14 @@ if (windowEnvironment === undefined) {
 }
 
 export const Constants = {
-    apiUrl:
-        (windowEnvironment.REACT_APP_API_URL 
-        || process.env.REACT_APP_API_URL),
-    isDev:
-        (windowEnvironment.ENV === 'development' ||
-        process.env.NODE_ENV === 'development'),
-    isProd:
-        (windowEnvironment.ENV === 'production' ||
-        process.env.NODE_ENV === 'production'),
-    idpUrl:
-        (windowEnvironment.REACT_APP_IDP_URL 
-        || process.env.REACT_APP_IDP_URL),
-    clientId: 
-        (windowEnvironment.CLIENT_ID 
-        || process.env.CLIENT_ID),
-    appUrl: 
-        (windowEnvironment.REACT_APP_URL 
-        || process.env.REACT_APP_URL),
+  apiUrl: windowEnvironment.REACT_APP_API_URL || process.env.REACT_APP_API_URL,
+  isDev:
+    windowEnvironment.ENV === "development" ||
+    process.env.NODE_ENV === "development",
+  isProd:
+    windowEnvironment.ENV === "production" ||
+    process.env.NODE_ENV === "production",
+  idpUrl: windowEnvironment.REACT_APP_IDP_URL || process.env.REACT_APP_IDP_URL,
+  clientId: windowEnvironment.CLIENT_ID || process.env.CLIENT_ID,
+  appUrl: windowEnvironment.REACT_APP_URL || process.env.REACT_APP_URL
 };
