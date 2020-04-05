89bbc08b92917ca92ac5e2dadf9a0263ac79f078
Mircea M <mircea.moise@gmail.com>
2020-04-05 18:18:13 +0100

    Styling for tabs

--- frontend/src/components/Evaluation/tabTitle.js
+++ frontend/src/components/Evaluation/tabTitle.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
+import "./tabTitle.scss";
 
 const TabTitle = ({ history, location: { pathname }, link, children }) => {
   return (
