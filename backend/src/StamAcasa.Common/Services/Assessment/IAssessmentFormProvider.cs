111cfa436b67b94c6ce52ce696a75aa2f41d66eb
ion.dormenco <ion.dormenco@maxcode.net>
2020-04-08 14:54:54 +0300

    Improve assessment service to read files from disk

---
+++ backend/src/StamAcasa.Common/Services/Assessment/IAssessmentFormProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace StamAcasa.Common.Services.Assessment
+{
+    public interface IAssessmentFormProvider
+    {
+        Task<string> GetFormNewUser();
+        Task<string> GetFormFollowUp();
+    }
+}
