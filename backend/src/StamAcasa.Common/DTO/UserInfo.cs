e9c294be3ced3123fadd24aeca718f6acdf5db64
irinel-nistor <irinel.nistor92@gmail.com>
2020-04-09 15:15:33 +0300

    Use EmailRequestModel in AssessmentNotificationsDispatch

--- backend/src/StamAcasa.Common/DTO/UserInfo.cs
+++ backend/src/StamAcasa.Common/DTO/UserInfo.cs
@@ -7,6 +7,7 @@ namespace StamAcasa.Common.DTO {
         [JsonIgnore]
         public int? ParentId { get; set; }
         public string Name { get; set; }
+        public string Email { get; set; }
         public string Surname { get; set; }
         public string PhoneNumber { get; set; }
         public string County { get; set; }
