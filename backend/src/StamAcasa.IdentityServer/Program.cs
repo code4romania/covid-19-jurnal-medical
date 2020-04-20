262531f7ee5e91791f147492ffebe2bde0e98c7a
ion.dormenco <ion.dormenco@maxcode.net>
2020-04-20 09:54:02 +0300

    Change logging to file logging #264
    closes #264

--- backend/src/StamAcasa.IdentityServer/Program.cs
+++ backend/src/StamAcasa.IdentityServer/Program.cs
@@ -1,33 +1,29 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Events;
 using Serilog.Sinks.SystemConsole.Themes;
-using StamAcasa.IdentityServer;
 
-namespace IdentityServer {
-    public class Program {
-        public static void Main(string[] args) {
+namespace IdentityServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string loggerTemplate = @"{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u4}]<{ThreadId}> [{SourceContext:l}] {Message:lj}{NewLine}{Exception}";
+            var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            var logfile = Path.Combine(baseDir, "logs", "log.txt");
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Verbose()
                 .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
                 .MinimumLevel.Override("System", LogEventLevel.Warning)
                 .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
                 .Enrich.FromLogContext()
-                // uncomment to write to Azure diagnostics stream
-                //.WriteTo.File(
-                //    @"D:\home\LogFiles\Application\identityserver.txt",
-                //    fileSizeLimitBytes: 1_000_000,
-                //    rollOnFileSizeLimit: true,
-                //    shared: true,
-                //    flushToDiskInterval: TimeSpan.FromSeconds(1))
+                .WriteTo.File(logfile, LogEventLevel.Information, loggerTemplate,
+                    rollingInterval: RollingInterval.Day, retainedFileCountLimit: 90)
                 .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate)
                 .CreateLogger();
             CreateHostBuilder(args).Build().Run();
