e86934497086b53972329f6bc90e6ab744a39b39
ion.dormenco <ion.dormenco@maxcode.net>
2020-04-20 10:48:30 +0300

    Update const.js
    closes #253

--- frontend/src/components/Footer/const.js
+++ frontend/src/components/Footer/const.js
@@ -37,7 +37,7 @@ export const FOOTER_LINKS = {
     }
   ],
   rightColumn: [
-    { URL: "https://cemafac.ro/", label: "Ce mă fac" },
+    { URL: "https://cetrebuiesafac.ro/", label: "Ce trebuie sa fac" },
     { URL: "https://datelazi.ro/", label: "Date la zi" },
     { URL: "https://diasporahub.ro/", label: "Diaspora Hub" },
     { URL: "https://rohelp.ro/", label: "Ro Help" },
