515642a2f31e73672f8f5ea3a7ac5f8eab559161
Teodora Dima <teo.dima@gmail.com>
2020-04-21 22:58:03 +0300

    #276 Comment out dashboard route

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -11,18 +11,17 @@ import {
   silentRefreshCallback
 } from "./api/auth";
 
-import BaseDashboard from "./components/BaseDashboard";
-
 export const ROUTES = {
   base: {
     despre: {
       path: "/despre",
       component: About
     },
-    dashboard: {
-      path: "/dashboard",
-      component: BaseDashboard
-    },
+    // TODO Uncomment when dashboard is needed again
+    // dashboard: {
+    //   path: "/dashboard",
+    //   component: BaseDashboard
+    // },
     home: {
       path: "/",
       component: Home
