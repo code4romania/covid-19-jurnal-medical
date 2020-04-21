e24622967cbbfde43b4fb0fe026f385538143be2
irinel-nistor <irinel.nistor92@gmail.com>
2020-04-21 10:54:22 +0300

    #280 Add evaluation link to daily assessment email

--- backend/src/StamAcasa.Common/Notifications/AssessmentNotificationsDispatch.cs
+++ backend/src/StamAcasa.Common/Notifications/AssessmentNotificationsDispatch.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using StamAcasa.Common.DTO;
+using StamAcasa.Common.Models;
 using StamAcasa.Common.Queue;
 using StamAcasa.Common.Services;
 using StamAcasa.Common.Services.Emailing;
@@ -13,11 +14,13 @@ namespace StamAcasa.Common.Notifications
         private IUserService UserService { get; }
 
         private readonly IQueueService _queueService;
+        private readonly UserProfileUrls _userProfileUrls;
 
-        public AssessmentNotificationsDispatch(IUserService userService, IQueueService queueService)
+        public AssessmentNotificationsDispatch(IUserService userService, IQueueService queueService, UserProfileUrls userProfileUrls)
         {
             UserService = userService;
             _queueService = queueService;
+            _userProfileUrls = userProfileUrls;
         }
 
         public async Task Process()
@@ -37,7 +40,8 @@ namespace StamAcasa.Common.Notifications
                 Type = "dailyAssessment",
                 PlaceholderContent = new Dictionary<string, string>
                 {
-                    { "name", $"{userInfo.Name} {userInfo.Surname}" }
+                    { "name", $"{userInfo.Name} {userInfo.Surname}" },
+                    { "evaluationLink", _userProfileUrls.Evaluation }
                 }
             };
         }
