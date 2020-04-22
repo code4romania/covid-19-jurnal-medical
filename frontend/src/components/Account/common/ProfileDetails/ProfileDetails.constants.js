1ad277dcf6fd4ff22b27c7c55cfa5598c8fe3d0c
ion.dormenco <ion.dormenco@maxcode.net>
2020-04-22 12:41:14 +0300

    Update options and constants
    closes #296

--- frontend/src/components/Account/common/ProfileDetails/ProfileDetails.constants.js
+++ frontend/src/components/Account/common/ProfileDetails/ProfileDetails.constants.js
@@ -39,7 +39,15 @@ const FIELDS_DICT = {
   quarantineStatusOthers: QUARANTINE_STATUS_DICT
 };
 
+const PREEXISTING_MEDICAL_CONDITION_MAP = {
+  6: "Nu exista"
+};
+
+const FIELDS_PARTIAL_MAP_DICT = {
+  preexistingMedicalCondition: PREEXISTING_MEDICAL_CONDITION_MAP
+}
 const fieldsWithDict = ["gender", "quarantineStatus", "quarantineStatusOthers"];
+const fieldsWithPartialMap = ["preexistingMedicalCondition"];
 
 const mapProfileDetails = data => {
   return Object.entries(FIELDS_LABELS)
@@ -61,7 +69,11 @@ const mapProfileDetails = data => {
       }
 
       if (fieldsWithDict.includes(key)) {
-        return { ...mappedItem, value: FIELDS_DICT[key][fieldValue] };
+        return { ...mappedItem, value: FIELDS_DICT[key][+fieldValue] };
+      }
+
+      if(fieldsWithPartialMap.includes(key) && FIELDS_PARTIAL_MAP_DICT[key][+fieldValue] !== undefined){
+        return { ...mappedItem, value: FIELDS_PARTIAL_MAP_DICT[key][+fieldValue] };
       }
 
       return mappedItem;
