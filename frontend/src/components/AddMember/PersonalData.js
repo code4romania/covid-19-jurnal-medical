5cfda9111cc1785c78dadf1c247156ff95bb317f
irinel-nistor <irinel.nistor92@gmail.com>
2020-04-25 17:31:21 +0300

    #262 Add custom validation messages

--- frontend/src/components/AddMember/PersonalData.js
+++ frontend/src/components/AddMember/PersonalData.js
@@ -27,6 +27,25 @@ export const PersonalData = ({
     });
   };
 
+  const nameValidationMessages = {
+    patternMismatch: "Acest camp poate contine doar caractere alfanumerice",
+    valueMissing: "Acest camp este obligatoriu"
+  };
+
+  const phoneValidationMessages = {
+    patternMismatch:
+      "Va rog introduceti un numar de telefon valid (07xxxxxxxx sau 00xxxxxxxxxx - doar cifre)",
+    valueMissing: "Acest camp este obligatoriu",
+    tooShort: "Acest camp trebuie sa contina minim 10 caractere"
+  };
+
+  const ageValidationMessages = {
+    rangeOverflow: "Valoarea trebuie sa fie mai mica sau egala cu 120",
+    rangeUnderflow: "Valoarea trebuie sa fie mai mare sau egala cu 0",
+    valueMissing: "Acest camp este obligatoriu",
+    stepMismatch: "Valoarea trebuie sa fie un numar intreg"
+  };
+
   return (
     <>
       <ListHeader title="I. Date personale" />
@@ -35,6 +54,8 @@ export const PersonalData = ({
           type="text"
           label={"Nume"}
           name="nume"
+          title="Nume"
+          validationMessages={nameValidationMessages}
           required
           pattern="[A-Za-z ]{1,32}"
           usePlaceholder
@@ -48,8 +69,10 @@ export const PersonalData = ({
           type="text"
           label={"Prenume"}
           name="surname"
+          title="Prenume"
           required
           pattern="[A-Za-z ]{1,32}"
+          validationMessages={nameValidationMessages}
           usePlaceholder
           value={userData.surname}
           defaultValue={userData.surname}
@@ -63,6 +86,7 @@ export const PersonalData = ({
           type="tel"
           pattern="(?:00|07)[0-9]*"
           title="07xxxxxxxx sau 00xxxxxxxxxx - doar cifre"
+          validationMessages={phoneValidationMessages}
           minLength="10"
           maxLength="13"
           required={!isForFamilyMember}
@@ -109,7 +133,9 @@ export const PersonalData = ({
           type="number"
           label={"Vârstă în ani împliniți"}
           name="age"
+          title="Vârstă"
           required
+          validationMessages={ageValidationMessages}
           usePlaceholder
           value={userData.age}
           defaultValue={userData.age}
