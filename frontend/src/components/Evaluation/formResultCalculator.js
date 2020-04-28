4dcb28cf7d5a29736483fc4dfc2b8a7841a37392
Mircea Moise <mircea.moise@gmail.com>
2020-04-28 20:26:58 +0100

    Added form result calculation (#397)

---
+++ frontend/src/components/Evaluation/formResultCalculator.js
@@ -0,0 +1,33 @@
+const formResultCalculator = (formState, options) => {
+  const scoreForId = questionId => {
+    return formState[questionId] ? 1 : 0;
+  };
+
+  const importantQuestionIds = [1, 3, 5, 7, 9, 20];
+  const coronaLikelihoodScore = importantQuestionIds.reduce(
+    (currentScore, currentId) => currentScore + scoreForId(currentId),
+    0
+  );
+
+  let responseValue = "R3";
+
+  if (coronaLikelihoodScore === 1) {
+    responseValue = "R2";
+  }
+
+  if (coronaLikelihoodScore > 1) {
+    responseValue = "R1";
+  }
+
+  const choosenResponse = options.find(
+    option => option.value === responseValue
+  );
+
+  if (!choosenResponse) {
+    return { label: "Mulțumim", value: 0 };
+  }
+
+  return choosenResponse;
+};
+
+export default formResultCalculator;
