ba649c95f7c9f6aad05894fa220e7678d97d0a6a
Mircea M <mircea.moise@gmail.com>
2020-04-30 13:10:24 +0100

    Handling the case when CSS.supports is not supported

--- frontend/src/components/SelectCentered/index.js
+++ frontend/src/components/SelectCentered/index.js
@@ -11,8 +11,6 @@ const SelectCentered = ({
   defaultValue,
   placeholder
 }) => {
-  //todo meta: what is CSS.supports is not supported??
-  const worksByDefault = CSS.supports("text-align-last", "center");
   const select = () => {
     return (
       <Select
@@ -25,16 +23,25 @@ const SelectCentered = ({
       />
     );
   };
-  if (worksByDefault) {
+
+  // a bit meta I know! But IE 11 doesn't support CSS.supports
+  // however it supports text-align-last hence why when CSS.supports
+  // is not supported it does the default behaviour
+  const cssSupportsSupported = CSS ? CSS.supports : false;
+
+  if (!cssSupportsSupported) {
+    return select();
+  }
+  const textCenteredByDefault = CSS.supports("text-align-last", "center");
+
+  if (textCenteredByDefault) {
     return select();
   } else {
     const selected = options.find(option => option.selected);
     return (
       <label className="center-select">
         {select()}
-        <span className="center-select__text">
-          {selected ? selected.text : placeholder}
-        </span>
+        <span className="text">{selected ? selected.text : placeholder}</span>
       </label>
     );
   }
