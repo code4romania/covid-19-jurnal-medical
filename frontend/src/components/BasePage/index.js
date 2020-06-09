15a7e145589a06f098fa211d8963f58e9a79a22e
Mircea Moise <mircea.moise@gmail.com>
2020-06-09 10:24:13 +0100

    Terms and conditions update (#448)
    * Terms and conditions content
    
    * Cookie consent
    
    * Added link for terms and conditions in the footer
    
    * Implementing PR feedback
    
    Co-authored-by: Radu Stefanescu <radu.stefanescu@code4.ro>

--- frontend/src/components/BasePage/index.js
+++ frontend/src/components/BasePage/index.js
@@ -4,12 +4,19 @@ import PropTypes from "prop-types";
 import Header from "../Header";
 import Footer from "../Footer";
 
+import CookieConsent from "react-cookie-consent";
+
 const BasePage = ({ children }) => {
   return (
     <>
       <Header />
       <div className="container main-content">{children}</div>
       <Footer />
+      <CookieConsent buttonText="Am înțeles" acceptOnScroll={true}>
+        Acest site folosește cookie-uri. Prin continuarea navigării vă exprimați
+        acordul pentru folosirea cookie-urilor, conform Regulamentului (UE)
+        2016/679.
+      </CookieConsent>
     </>
   );
 };
