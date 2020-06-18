8cee895ec44acbcb5ef803c7634ab2b032a069b3
ion.dormenco <ion.dormenco@maxcode.net>
2020-06-18 14:14:12 +0300

    Redirect user to a verified url
    After user confirms email we should check url where we should redirect him.
    
    See unit tests
    closes #436

--- backend/src/StamAcasa.IdentityServer/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ backend/src/StamAcasa.IdentityServer/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -1,11 +1,18 @@
-﻿using System.Text;
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 using IdentityServer.Data;
+using IdentityServer4.Extensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
+using StamAcasa.IdentityServer;
+using StamAcasa.IdentityServer.Helpers;
+using StamAcasa.IdentityServer.Options;
 
 namespace IdentityServer.Pages.Account
 {
@@ -14,11 +21,27 @@ namespace IdentityServer.Pages.Account
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly List<Uri> _allowedRedirectUrls;
 
-        public ConfirmEmailModel(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        public ConfirmEmailModel(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, AllowedRedirects allowedRedirects)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _allowedRedirectUrls = BuildAllowedRedirectUrls(allowedRedirects);
+        }
+
+        private static List<Uri> BuildAllowedRedirectUrls(AllowedRedirects allowedRedirects)
+        {
+            if (allowedRedirects?.Urls == null)
+            {
+                throw new ArgumentNullException(nameof(allowedRedirects));
+            }
+            if (!allowedRedirects.Urls.Any())
+            {
+                throw new ArgumentException("Please provide at least 1 allowed redirect");
+            }
+
+            return allowedRedirects.Urls.Select(x => new Uri(UrlHelpers.NormalizeUrl(x))).ToList();
         }
 
         [TempData]
@@ -30,7 +53,7 @@ namespace IdentityServer.Pages.Account
 
         public async Task<IActionResult> OnGetAsync(string userId, string code, string returnUrl)
         {
-            if (userId == null || code == null)
+            if (string.IsNullOrEmpty(userId) || string.IsNullOrEmpty(code))
             {
                 return RedirectToPage("/Index");
             }
@@ -44,14 +67,35 @@ namespace IdentityServer.Pages.Account
             code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
             var result = await _userManager.ConfirmEmailAsync(user, code);
             Success = result.Succeeded;
-            RedirectUrl = returnUrl;
+            RedirectUrl = GetRedirectUrl(returnUrl);
             StatusMessage = result.Succeeded ?
                 "Mulțumim pentru confirmarea adresei de e-mail. Vei fi redirecționat(ă) către aplicație." :
                 "Eroare la confirmarea adresei de email.";
+
             if (!string.IsNullOrEmpty(returnUrl))
                 await _signInManager.SignInAsync(user, isPersistent: false);
 
             return Page();
+        }
+
+        private string GetRedirectUrl(string returnUrl)
+        {
+            var defaultRedirectUrl = Url.Content("~/");
+
+            if (returnUrl.IsNullOrEmpty())
+            {
+                return defaultRedirectUrl;
+            }
+
+            Uri returnUrlUri = new Uri(UrlHelpers.NormalizeUrl(returnUrl));
+
+            if (_allowedRedirectUrls.Any(x => x == returnUrlUri))
+            {
+                return returnUrl;
+            }
+
+
+            return defaultRedirectUrl;
         }
     }
 }
