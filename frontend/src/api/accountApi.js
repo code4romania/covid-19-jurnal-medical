d0271c703716770c0d443b9d02ecbc4717a2e345
Mircea M <mircea.moise@gmail.com>
2020-06-27 14:21:08 +0100

    Using the identity server URL

--- frontend/src/api/accountApi.js
+++ frontend/src/api/accountApi.js
@@ -1,4 +1,9 @@
-import api from "./api";
+import axios from "axios";
+import { Constants } from "../config/constants";
+
+const api = axios.create({
+  baseURL: `${Constants.idpUrl}/api`
+});
 
 const AccountApi = {
   deleteAccount: (user, password) =>
