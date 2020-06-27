36627d5aefa24cc5aacea347d28b2088dd563d06
Gabriel Majeri <gabriel.majeri6@gmail.com>
2020-06-27 17:17:30 +0300

    Only show install extension CTA if matching brower

--- frontend/src/components/SidebarLayout/index.js
+++ frontend/src/components/SidebarLayout/index.js
@@ -5,24 +5,31 @@ import {
   InstrumentsItem,
   Hero
 } from "@code4ro/taskforce-fe-components";
-import { instrumentsItems } from "./const";
+import { browserExtensions, instrumentsItems } from "./const";
 
 const SidebarLayout = ({ children }) => {
+  // Browser detection based on this answer:
+  // https://stackoverflow.com/a/9851769/5723188
+  const isChrome =
+    !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+  const isFirefox = typeof InstallTrigger !== "undefined";
+
+  let installExtensionItem = null;
+  if (isChrome) {
+    installExtensionItem = <InstrumentsItem {...browserExtensions.chrome} />;
+  } else if (isFirefox) {
+    installExtensionItem = <InstrumentsItem {...browserExtensions.firefox} />;
+  }
+
   return (
     <div className="columns">
       <div className="column is-8">{children}</div>
       <aside className="column is-4">
         <Instruments layout="column">
           <Hero useFallbackIcon title="Instrumente utile" />
+          {installExtensionItem}
           {instrumentsItems.map((item, index) => (
-            <InstrumentsItem
-              key={index}
-              color={item.color}
-              title={item.title}
-              content={item.content}
-              ctaText={item.ctaText}
-              ctaLink={item.ctaLink}
-            />
+            <InstrumentsItem key={index} {...item} />
           ))}
         </Instruments>
       </aside>
