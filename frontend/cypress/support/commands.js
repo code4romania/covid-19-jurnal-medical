b72e6d33c52a54ffe0574d575e8ac26f48b95004
Utwo <mihai.legat@gmail.com>
2020-07-12 16:40:08 +0300

    Remove cypress

--- frontend/cypress/support/commands.js
+++
@@ -1,9 +0,0 @@
-Cypress.Commands.add("routeUseAuth", (options, token) => {
-  options.onRequest = xhr => {
-    expect(xhr.request.headers).contain({
-      Authorization: `Bearer ${token}`
-    });
-  };
-
-  cy.route(options);
-});
