576f3182f193f0d60bd5ac59981c390e3d438f76
Ion Dormenco <idormenco@gmail.com>
2020-07-31 15:50:00 +0300

    linter fix

--- frontend/src/components/DeleteAccount/index.js
+++ frontend/src/components/DeleteAccount/index.js
@@ -31,9 +31,9 @@ const DeleteAccount = () => {
       <div>
         Contul tău va fi șters. Pentru a putea reutiliza această aplicație va
         trebui să îți refaci contul de utilizator. Informațiile pe care le-ai
-        transmis până acum prin intermediul aplicației vor rămâne stocate în baza
-        de date. Dacă dorești ca toate informațiile să fie eliminate din baza de
-        date te rugăm să adresezi această cerere către:
+        transmis până acum prin intermediul aplicației vor rămâne stocate în
+        baza de date. Dacă dorești ca toate informațiile să fie eliminate din
+        baza de date te rugăm să adresezi această cerere către:
         <p>Adresa: Strada Italiană, nr. 22, Sector 2, 020976, București</p>
         <p>E-mail: jurnalmedical@adr.gov.ro</p>
       </div>
